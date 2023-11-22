a9d489941a0aae4ee66241f5073b1921cd1746bc
LuceroGustavo <129872263+LuceroGustavo@users.noreply.github.com>
2023-11-22 15:33:06 -0300

    4 COMIT GUSTAVO
    creacion de IMG en clases

--- proyectoFinal/src/main/java/com/example/proyecto/entidades/Imagen.java
+++ proyectoFinal/src/main/java/com/example/proyecto/entidades/Imagen.java
@@ -1,7 +1,5 @@
 /*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
+GUSTAVVO 22/11
  */
 package com.example.proyecto.entidades;
 
